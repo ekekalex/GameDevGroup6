0778c2b9c2c30a827e10654d23341fbce2f43608
br4ndonn <105334555@student.swin.edu.au>
2025-05-20 23:22:11 +1000

    Imported Background Menu Music

---
+++ Group6-Game/Assets/Menu Assets/MainMenu.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame ()
+    {
+        Debug.Log ("Quit");
+        Application.Quit();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+// {
+//     // Start is called once before the first execution of Update after the MonoBehaviour is created
+//     void Start()
+//     {
+        
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+        
+//     }
+// }

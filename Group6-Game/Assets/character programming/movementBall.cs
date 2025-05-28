de691339efedf6f9ffeb34b0ff987149548a158b
amywamy <105334607@student.swin.edu.au>
2025-05-28 23:36:16 +1000

    Update movementBall.cs

--- Group6-Game/Assets/character programming/movementBall.cs
+++ Group6-Game/Assets/character programming/movementBall.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class MovementBall : MonoBehaviour
 {
     private float rollForce = 4f;
+    private bool allowForwardOnly = true;
 
     private Rigidbody rb;
     private void Awake()
@@ -13,10 +14,14 @@ public class MovementBall : MonoBehaviour
 
     private void FixedUpdate()
     {
-        float h = Input.GetAxis("Horizontal");
         float c = Input.GetAxis("Vertical");
-
-        Vector3 move = new Vector3(h, 0, c);
+        if (allowForwardOnly && c <= 0f)
+        {
+            return;
+        }
+        Vector3 move = new Vector3(0, 0, 1) * c;
+        rb.linearVelocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
         rb.AddTorque(move * -1);
     } 
 

44edfc1bca2d133a593fcbc15210aa2b73552350
amywamy <105334607@student.swin.edu.au>
2025-06-02 14:11:07 +1000

    Update cameraControl.cs

--- Group6-Game/Assets/character programming/cameraControl.cs
+++ Group6-Game/Assets/character programming/cameraControl.cs
@@ -4,10 +4,10 @@ public class CameraControl : MonoBehaviour
 {
     //Target character
     public Transform target;
-    public Vector3 offset = new Vector3(0, 2, -6);
+    public Vector3 offset = new Vector3(0, 2, -6f);
 
     //Orbit rotation
-    public float mouseSensitivity = 1.5f;
+    public float mouseSensitivity = 2f;
     public float pitch = 20f;
     public float yaw = 0f;
     public float minPitch = -20f;
@@ -17,11 +17,16 @@ public class CameraControl : MonoBehaviour
     private float zoomSpeed = 1f;
     public float minZoom = 3f;
     public float maxZoom = 10f;
-    public float followSpeed = 3f;
-    private Vector3 velocity = Vector3.zero;
+    private Vector3 currentVelocity;
+    private Vector3 finalPosition;
+    private Quaternion finalRotation;
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        yaw = target.eulerAngles.y;
+        finalPosition = transform.position;
+        finalRotation = transform.rotation;
     }
 
     private void LateUpdate()
@@ -34,18 +39,25 @@ public class CameraControl : MonoBehaviour
         HandleZoom();
 
         Quaternion rotation = Quaternion.Euler(pitch, yaw, 0f);//cam rotate base on mouse input
-        Vector3 desiredPosition = target.position + rotation * offset.normalized *currentZoom; //cal new cam position from offset and rotation
-        transform.position = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, 0.1f);;
-        transform.LookAt(target.position + Vector3.up * 1.3f);
+        Vector3 desiredPosition = target.position + rotation * offset.normalized * currentZoom; //cal new cam position from offset and rotation
+        finalPosition = Vector3.SmoothDamp(finalPosition, desiredPosition, ref currentVelocity, 0.1f);
+        finalRotation = Quaternion.LookRotation(target.position + Vector3.up * 1.3f - finalPosition);
+        transform.position = finalPosition;
+        transform.rotation = finalRotation;
     }
 
     private void HandleRotationInput()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
-        yaw += mouseX;
-        pitch -= mouseY;
-        pitch = Mathf.Clamp(pitch, minPitch, maxPitch);
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        if (Mathf.Abs(mouseX) > 0.01f || Mathf.Abs(mouseY) > 0.01f)
+        {
+            yaw += mouseX * mouseSensitivity;
+            pitch -= mouseY * mouseSensitivity;
+            pitch = Mathf.Clamp(pitch, minPitch, maxPitch);
+        }
+
     }
 
     private void HandleZoom()

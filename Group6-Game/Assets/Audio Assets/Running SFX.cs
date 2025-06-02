cf0285be84c41c461411afcfd833267fff5ce116
ekekalex <ekytoras@gmail.com>
2025-06-02 22:28:52 +1000

    more lights and fog adjustments

--- Group6-Game/Assets/Audio Assets/Running SFX.cs
+++ Group6-Game/Assets/Audio Assets/Running SFX.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class footsteps : MonoBehaviour
 {
     public AudioSource footstepsSound;
+    private CharacterController characterController;
 
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
     void Update()
     {
-        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D)){
+        if((Input.GetKey(KeyCode.W) | Input.GetKey(KeyCode.A) | Input.GetKey(KeyCode.S) | Input.GetKey(KeyCode.D)) && characterController.isGrounded)
+        {
             footstepsSound.enabled = true;
         }
         else
